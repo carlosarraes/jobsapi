3f626cb8d9457ef64128fbb3abffb709fca8a180
carraes <carraeshb@gmail.com>
2022-11-16 11:12:52 -0300

    chore: security packages

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+//! Security Packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
+)
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // Connect DB
 const connectDB = require('./db/connect')
